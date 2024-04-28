e0535437c59a3183fae06466e5d573849f974fd0
EbrahimkhanM <ebrahimkhaanm@gmail.com>
2024-04-29 03:59:37 +0500

    search api updated more source one

---
+++ src/app/api/source-news/route.js
@@ -0,0 +1,27 @@
+import { NextResponse, NextApiRequest } from "next/server";
+export const corsHeaders = {
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+  "Access-Control-Allow-Headers": "Content-Type, Authorization",
+};
+export async function GET(request, response) {
+  const { searchParams } = new URL(request.url);
+  const  sources = searchParams.get("sources");
+  const  apiKey = searchParams.get("apiKey");
+  try {
+    const response = await fetch(
+        `https://newsapi.org/v2/top-headlines?sources=${sources}&apiKey=${apiKey}`
+    );
+
+    if (!response.ok) {
+      throw new Error("Network response was not ok");
+    }
+
+    const responseData = await response.json();
+
+    return NextResponse.json(responseData, { headers: corsHeaders });
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    return NextResponse.error(new Error("Failed to fetch data"));
+  }
+}
