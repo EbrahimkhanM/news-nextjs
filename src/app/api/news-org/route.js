82bc57f2a95bdf3495dc0b2b94a05636933bda93
salmank96 <salmankhan187020@gmail.com>
2024-07-28 13:52:16 +0500

    fixes

--- src/app/api/news-org/route.js
+++ src/app/api/news-org/route.js
@@ -13,7 +13,7 @@ export async function GET(request, response) {
   const  toDate = searchParams.get("toDate") ?? "";
   try {
     const response = await fetch(
-      `https://newsapi.org/v2/top-headlines?q=${q}&sources=${sources}&from=${fromDate}&to=${toDate}&apiKey=${apiKey}&language=en&searchIn=title`
+      `https://newsapi.org/v2/everything?q=${q ? q: "apple"}&sources=${sources}&from=${fromDate}&to=${toDate}&apiKey=${apiKey}&language=en&searchIn=title`
     );
 
     if (!response.ok) {
